91d3482e0a2062bebcda0368cb06262852be7071
SALY ABBO <elmaroufa@gmail.com>
2022-07-07 09:51:04 +0100

    Update exercise2.py

--- exercise2.py
+++ exercise2.py
@@ -1,1 +1,11 @@
 #New file add ohter exercic
+def solution(number):
+    listes = range(number)
+    listes = [ int(x) for x in listes] 
+    som = 0
+    for x in listes:
+        if ( x%3 == 0 or x%5 == 0):
+            som = som + x
+    return som
+  
+  
