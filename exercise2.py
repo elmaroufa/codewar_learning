e5b77c324a9fde8d2c3e284b4124cabdb24ffd63
SALY ABBO <elmaroufa@gmail.com>
2022-07-08 15:53:11 +0100

    Update exercise2.py

--- exercise2.py
+++ exercise2.py
@@ -20,3 +20,19 @@ def solution(number):
             sum += i
     return sum
 #--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#--------------------------------------------------------------------EXERCICE EXTRACT DOMAIN IN URL -----------------------------------------------------------------
+
+import re
+def domain_name(url):
+    pattern = re.compile(r'(https?://|www\.)?(www\.)?([a-z0-9-]+)(\..+)?')
+    subbed_url = pattern.sub(r'\3',url)
+    return subbed_url
+#--------------------------------------------------------------------------OTHER SOLUTION--------------------------------------------------------------------------
+def domain_name(url):
+    return url.split("//")[-1].split("www.")[-1].split(".")[0]
+
+import re
+def domain_name(url):
+    return re.search('(https?://)?(www\d?\.)?(?P<name>[\w-]+)\.', url).group('name')
+
